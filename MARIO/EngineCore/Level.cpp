c0b77c637a781b8b93d7d59cb3655bdbea167f09
jeongwoo 조정우 <choeun1999@gmail.com>
2024-02-06 16:48:11 +0900

    조정우 : 마리오 이동 구현 커밋

---
+++ MARIO/EngineCore/Level.cpp
@@ -0,0 +1,121 @@
+#include "Level.h"
+#include "Actor.h"
+#include <EngineBase\EngineDebug.h>
+
+ULevel::ULevel()
+{
+}
+
+ULevel::~ULevel()
+{
+	for (std::pair<const int, std::list<AActor*>>& OrderListPair : AllActor)
+	{
+		std::list<AActor*>& ActorList = OrderListPair.second;
+		for (AActor* Actor : ActorList)
+		{
+			if (nullptr == Actor)
+			{
+				continue;
+			}
+
+			delete Actor;
+			Actor = nullptr;
+		}
+	}
+	AllActor.clear();
+}
+
+void ULevel::LevelTick(float _DeltaTime)
+{
+	for (std::pair<const int, std::list<AActor*>>& OrderListPair : AllActor)
+	{
+		std::list<AActor*>& ActorList = OrderListPair.second;
+		for (AActor* Actor : ActorList)
+		{
+			if (false == Actor->IsActive())
+			{
+				continue;
+			}
+
+			Actor->Tick(_DeltaTime);
+		}
+	}
+}
+
+void ULevel::LevelRender(float _DeltaTime)
+{
+	for (std::pair<const int, std::list<UImageRenderer*>>& OrderListPair : Renderers)
+	{
+		std::list<UImageRenderer*>& RendererList = OrderListPair.second;
+		for (UImageRenderer* Renderer : RendererList)
+		{
+			if (false == Renderer->IsActive())
+			{
+				continue;
+			}
+
+			Renderer->Render(_DeltaTime);
+		}
+	}
+
+}
+
+void ULevel::LevelRelease(float _DeltaTime)
+{
+	for (std::pair<const int, std::list<UImageRenderer*>>& OrderListPair : Renderers)
+	{
+		std::list<UImageRenderer*>& RendererList = OrderListPair.second;
+
+		std::list<UImageRenderer*>::iterator StartIter = RendererList.begin();
+		std::list<UImageRenderer*>::iterator EndIter = RendererList.end();
+
+		for (; StartIter != EndIter; )
+		{
+			UImageRenderer* Renderer = StartIter.operator*();
+
+			if (false == Renderer->IsDestroy())
+			{
+				++StartIter;
+				continue;
+			}
+
+			StartIter = RendererList.erase(StartIter);
+		}
+	}
+	
+	for (std::pair<const int, std::list<AActor*>>& OrderListPair : AllActor)
+	{
+		std::list<AActor*>& ActorList = OrderListPair.second;
+
+		std::list<AActor*>::iterator StartIter = ActorList.begin();
+		std::list<AActor*>::iterator EndIter = ActorList.end();
+
+		for (; StartIter != EndIter;)
+		{
+			AActor* Actor = StartIter.operator*();
+
+			if (nullptr == Actor)
+			{
+				MsgBoxAssert("Actor가 nullptr인 경우가 존재했습니다");
+				return;
+			}
+
+			if (false == Actor->IsDestroy())
+			{
+				++StartIter;
+				continue;
+			}
+
+			delete Actor;
+			Actor = nullptr;
+			StartIter = ActorList.erase(StartIter);
+		}
+	}
+}
+
+
+void ULevel::ActorInit(AActor* _NewActor)
+{
+	_NewActor->SetWorld(this);
+	_NewActor->BeginPlay();
+}
